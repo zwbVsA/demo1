e1fab8feb90f77ff6ffbd99ce2f8db13cc6d996a
wenbo.zhang <wenbo.zhang@huifu.com>
2024-02-18 23:12:52 +0800

    修改

--- ch02/2.4.py
+++ ch02/2.4.py
@@ -1,3 +1,5 @@
+
+
 print("牧童骑黄牛","歌声振林樾")
 print("牧童骑黄牛","歌声振林樾",sep='*')
 print("牧童骑黄牛","歌声振林樾",end='>')
@@ -6,3 +8,6 @@ print("牧童骑黄牛","歌声振林樾")
 x=100
 print("x=%d" % x)
 print("x=%d" ,x)
+
+import keyword
+print(keyword.kwlist)

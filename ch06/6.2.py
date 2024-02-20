364f854280356c48d6d7448aebe5b029032063fe
wenbo.zhang <wenbo.zhang>
2024-02-20 20:35:54 +0800

    增加用例

--- ch06/6.2.py
+++ ch06/6.2.py
@@ -1,7 +1,18 @@
 def ss(x, y):
     """x + y"""
+    print('函数before：',x,y,id(x),id(y))
+    c=x
+    x=y
+    y=c
+    #x, y = y, x
+    print('函数after：', x, y,id(x),id(y))
     return x + y
 
 
-ds = ss(100, 4)
+x = 100
+y = 4
+print(type(x),type(y),id(x),id(y))
+print(x,y)
+ds = ss(x, y)
+print(x,y)
 print(ds)
